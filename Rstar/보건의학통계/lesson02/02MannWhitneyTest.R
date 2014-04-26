5d37d84ba7af048c76a9e13b65d01e803c17d072
junho <pinedance@mail.com>
2014-04-26 12:40:41 +0900

    / ‘Rstar/보건의학통계/lesson02/02MannWhitneyTest.R’

--- Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
+++ Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
@@ -3,3 +3,9 @@ data.B <- c(22, 16, 28, 23)
 
 # Mann-Whitney-Wilcoxon Test([src](http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test))
 
+data <- data.frame(data.A, data.B)
+stack(data.A, data.B)
+tmp.A <- merge("A", data.A)
+tmp.B <- merge("B", data.A)
+rbind(tmp.A, tmp.B)
+
