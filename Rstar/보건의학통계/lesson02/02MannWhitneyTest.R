7027963ab0cf170678a6869800b814dc2e11a0fc
junho <pinedance@mail.com>
2014-04-26 12:43:14 +0900

    / ‘Rstar/보건의학통계/lesson02/02MannWhitneyTest.R’

--- Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
+++ Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
@@ -8,9 +8,9 @@ data.B <- c(22, 16, 28, 23)
 # Mann-Whitney-Wilcoxon Test([src](http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test))
 
 ## data 만들기
-data <- data.frame(data.A, data.B)
 tmp.A <- merge("A", data.A)
-tmp.B <- merge("B", data.A)
+tmp.B <- merge("B", data.B)
 data <- rbind(tmp.A, tmp.B)
+data
 
 wilcox.test(y~x, data=data) 
