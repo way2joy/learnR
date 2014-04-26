ab37797d8d1db1ae090e98a5013199996bbf032f
junho <pinedance@mail.com>
2014-04-26 12:41:15 +0900

    / ‘Rstar/보건의학통계/lesson02/02MannWhitneyTest.R’

--- Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
+++ Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
@@ -1,8 +1,13 @@
+# <그림으로 이해하는 닥터 배의 술술 보건의학통계> p.65
+
+## 주어진 데이터
+
 data.A <- c(10, 12, 15, 22,21)
 data.B <- c(22, 16, 28, 23)
 
 # Mann-Whitney-Wilcoxon Test([src](http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test))
 
+## data 만들기
 data <- data.frame(data.A, data.B)
 tmp.A <- merge("A", data.A)
 tmp.B <- merge("B", data.A)
