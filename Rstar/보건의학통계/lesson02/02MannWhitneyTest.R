f834c20e6179a388e17a41dad1e6d30aea85979b
junho <pinedance@mail.com>
2014-04-26 12:32:03 +0900

    / ‘Rstar/보건의학통계/lesson02/02MannWhitneyTest.R’

--- Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
+++ Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
@@ -1,3 +1,5 @@
 data.A <- c(10, 12, 15, 22,21)
 data.B <- c(22, 16, 28, 23)
 
+# Mann-Whitney-Wilcoxon Test([src](http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test))
+
