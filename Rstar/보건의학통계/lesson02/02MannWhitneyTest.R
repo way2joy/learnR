e875b87f545398622e7d80656544f32298ed64b7
junho <pinedance@mail.com>
2014-04-26 12:44:47 +0900

    / ‘Rstar/보건의학통계/lesson02/02MannWhitneyTest.R’

--- Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
+++ Rstar/보건의학통계/lesson02/02MannWhitneyTest.R
@@ -3,14 +3,14 @@
 ## 주어진 데이터
 
 data.A <- c(10, 12, 15, 22,21)
-data.B <- c(22, 16, 28, 23)
+data.B <- c(23, 16, 28, 23)
 
 # Mann-Whitney-Wilcoxon Test([src](http://www.r-tutor.com/elementary-statistics/non-parametric-methods/mann-whitney-wilcoxon-test))
 
 ## data 만들기
 tmp.A <- merge("A", data.A)
 tmp.B <- merge("B", data.B)
-data <- rbind(tmp.A, tmp.B)
-data
+group <- as.data.frame(rbind(tmp.A, tmp.B))
+group
 
 wilcox.test(y~x, data=data) 
