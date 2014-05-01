2b351f612930e07239c4d4b3eae77ce9bd19f271
junho <pinedance@mail.com>
2014-05-01 20:49:14 +0900

    / ‘Rstar/보건의학통계/lesson03/02wilcoxon_signed_rank_test.R’

--- Rstar/보건의학통계/lesson03/02wilcoxon_signed_rank_test.R
+++ Rstar/보건의학통계/lesson03/02wilcoxon_signed_rank_test.R
@@ -16,6 +16,7 @@ plot(density(data$pre))
 lines(density(data$post), lty=2)
 
 boxplot(data$pre, data$post, col="yellow3")
+segments(1, data$pre, 2, data$post, col=1, lwd=0.25)
 
 ## wilcoxon signed rank test 값을 구해보자
 library('car')
