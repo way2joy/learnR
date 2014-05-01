1dba9126f1056d812e0e337f61ab6feb6325c839
junho <pinedance@mail.com>
2014-05-01 20:14:56 +0900

    / ‘Rstar/보건의학통계/lesson03/01paried_t_test.R’

--- Rstar/보건의학통계/lesson03/01paried_t_test.R
+++ Rstar/보건의학통계/lesson03/01paried_t_test.R
@@ -2,3 +2,9 @@
 setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson03")
 data <- read.csv("3_paired_t_test.csv", header=T)
 
+data$deviation <- data[2]-data[1]
+names(data$deviation) <- c("deviation")
+head(data)
+
+
+t.test()
