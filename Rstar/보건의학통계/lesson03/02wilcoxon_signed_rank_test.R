2237408a9a71112b3b7a376d65fb8bba2d26f8d1
junho <pinedance@mail.com>
2014-05-01 20:39:30 +0900

    / ‘Rstar/보건의학통계/lesson03/02wilcoxon_signed_rank_test.R’

--- Rstar/보건의학통계/lesson03/02wilcoxon_signed_rank_test.R
+++ Rstar/보건의학통계/lesson03/02wilcoxon_signed_rank_test.R
@@ -9,6 +9,16 @@ data <- read.csv("3_wilcoxon_signed_rank_test.csv", header=T)
 ## 정규성 검정 : 표본 수가 작으므로 (30이하) 정규성이 업다고 판단하고 정규성 검정을 하지 않는다. 하지만 정규성 검정을 하면 정규성을 따른다고 나온다. 
 shapiro.test(data$pre)  # p=0.8244 > 0.05 정규성 가진다.
 shapiro.test(data$post) # p=0.7282 > 0.05 정규성 가진다.
+
+## wilcoxon signed rank test
+### 그림으로 살펴보자.
+plot(density(data$pre))
+lines(density(data$post), lty=2)
+
+## ## 정규성 검정
+library(nortest)
+shapiro.test(data$pre)  # p=0.5652 > 0.05 정규성 가진다.
+shapiro.test(data$post) # p=0.8053 > 0.05 정규성 가진다.
 shapiro.test(data$deviation) # p=0.2466 > 0.05 정규성 가진다.
 
 ## 대응표본 t-test
@@ -16,7 +26,6 @@ shapiro.test(data$deviation) # p=0.2466 > 0.05 정규성 가진다.
 ### 그림으로 살펴보자.
 plot(density(data$pre))
 lines(density(data$post), lty=2)
-
-
+ 값을 구해보자
 library('car')
 wilcox.test(data$pre, data$post, paired=T) # p=0.2328 > 0.5
