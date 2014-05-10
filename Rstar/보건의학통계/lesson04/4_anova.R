2fd26821d7973260295101d3f6232715af0f8177
junho <pinedance@mail.com>
2014-05-10 12:38:33 +0900

    / ‘Rstar/보건의학통계/lesson04/4_anova.R’

--- Rstar/보건의학통계/lesson04/4_anova.R
+++ Rstar/보건의학통계/lesson04/4_anova.R
@@ -1,17 +1,7 @@
 setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson04")
 # getwd()
 
-read.csv("4_anova.csv")
+dGroup = read.csv("4_anova.csv", header=T)
+head(dGroup) ; str(dGroup)
+dGroup$group <- as.factor(dGroup$group)
 
-setwd("/home")
-
-setwd(parent.frame(2)$ofile)
-
-sys.frame(1)$ofile
-
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(this.dir)
-knitr:::input_dir()
-setwd (my.own.path ()) 
-
-script.dir <- dirname(sys.frame(1)$ofile)
