c39cbad18a5b8ea39867cc0c7f3311fbf4d3626f
junho <pinedance@mail.com>
2014-05-10 13:36:00 +0900

    / ‘Rstar/보건의학통계/lesson04/4_JT_test.R’

--- Rstar/보건의학통계/lesson04/4_JT_test.R
+++ Rstar/보건의학통계/lesson04/4_JT_test.R
@@ -1,6 +1,7 @@
-
+setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson04")
 
 dGroup = read.csv("4_JT_test.csv", header=T)
 head(dGroup) ; str(dGroup)
 dGroup$group <- as.factor(dGroup$group)
 plot(dGroup)
+
