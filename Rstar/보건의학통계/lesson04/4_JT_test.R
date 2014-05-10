06cf1880cae3232a9f32829e462018e92073a790
junho <pinedance@mail.com>
2014-05-10 14:24:05 +0900

    / ‘Rstar/보건의학통계/lesson04/4_JT_test.R’

--- Rstar/보건의학통계/lesson04/4_JT_test.R
+++ Rstar/보건의학통계/lesson04/4_JT_test.R
@@ -2,6 +2,11 @@ setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson04")
 
 dGroup = read.csv("4_JT_test.csv", header=T)
 head(dGroup) ; str(dGroup)
-dGroup$group <- as.factor(dGroup$group)
+# dGroup$group <- as.factor(dGroup$group)
 plot(dGroup)
+
+# install.packages("clinfun")
+library(clinfun)
+
+jonckheere.test(dGroup$score, dGroup$group)
 
