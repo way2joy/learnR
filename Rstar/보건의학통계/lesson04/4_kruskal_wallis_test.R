edc3fce66e9c59b18c43d66e6bf2f61d4d8d9212
junho <pinedance@mail.com>
2014-05-10 13:20:22 +0900

    / ‘Rstar/보건의학통계/lesson04/4_kruskal_wallis_test.R’

---
+++ Rstar/보건의학통계/lesson04/4_kruskal_wallis_test.R
@@ -0,0 +1,10 @@
+setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson04")
+
+dGroup = read.csv("4_kruskal_wallis_test.csv", header=T)
+head(dGroup) ; str(dGroup)
+dGroup$group <- as.factor(dGroup$group)
+plot(dGroup)
+
+# 정규성 검증
+tapply(dGroup$score, dGroup$group, shapiro.test)
+# http://acpi.tistory.com/33
