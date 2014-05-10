e1ac219deaeab7a836fdd634f27a0b9a4c7f229a
junho <pinedance@mail.com>
2014-05-10 12:34:47 +0900

    / ‘Rstar/보건의학통계/lesson04/4_anova.R’

--- Rstar/보건의학통계/lesson04/4_anova.R
+++ Rstar/보건의학통계/lesson04/4_anova.R
@@ -1,1 +1,17 @@
-read.csv("")
+setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson04")
+# getwd()
+
+read.csv("4_anova.csv")
+
+setwd("/home")
+
+setwd(parent.frame(2)$ofile)
+
+sys.frame(1)$ofile
+
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+knitr:::input_dir()
+setwd (my.own.path ()) 
+
+script.dir <- dirname(sys.frame(1)$ofile)
