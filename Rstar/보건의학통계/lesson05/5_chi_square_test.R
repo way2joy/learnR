0f1f5079e73bb0f3f4c4d411ab67c90e26d5a84f
junho <pinedance@mail.com>
2014-05-17 10:36:11 +0900

    / ‘Rstar/보건의학통계/lesson05/5_chi_square_test.R’

--- Rstar/보건의학통계/lesson05/5_chi_square_test.R
+++ Rstar/보건의학통계/lesson05/5_chi_square_test.R
@@ -1,7 +1,11 @@
 # http://dermabae.tistory.com/173
+# http://mkseo.pe.kr/stats/?p=300
 
 setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson05")
 
 data = read.csv("5_chi_square_test.csv", header=T)
 head(data)
+data$obesity <- as.factor(data$obesity)
+data$diabetes <- as.factor(data$diabetes)
 
+chisq.test(data$obesity, data$diabetes)
