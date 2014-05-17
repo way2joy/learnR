812ff36249eaaa94b25d8ac4a5c822551f3f89aa
junho <pinedance@mail.com>
2014-05-17 10:42:55 +0900

    / ‘Rstar/보건의학통계/lesson05/5_fisher_exact_test.R’

--- Rstar/보건의학통계/lesson05/5_fisher_exact_test.R
+++ Rstar/보건의학통계/lesson05/5_fisher_exact_test.R
@@ -2,3 +2,9 @@
 # http://mkseo.pe.kr/stats/?p=300
 
 setwd("~/SparkleShare/learnR/Rstar/보건의학통계/lesson05")
+
+data2 = read.csv("5_fisher_exact_test.csv", header=T)
+head(data2)
+data2$obesity <- as.factor(data2$obesity)
+data2$diabetes <- as.factor(data2$diabetes)
+
